84e2b02a0ff9a21fb4ba95e123b32f23ef7e12cb
Vanessa <vansouferr@gmail.com>
2025-02-01 21:01:20 -0300

    fiz erro markdown

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -2,13 +2,7 @@ import { marked } from 'marked';
 
 marked.setOptions({
   breaks: true,
-  gfm: true,
-  headerIds: false,
-  mangle: false,
-  sanitize: false,
-  smartLists: true,
-  smartypants: true,
-  xhtml: false
+  gfm: true
 });
 
 // Configurar o marked para abrir links em nova guia
